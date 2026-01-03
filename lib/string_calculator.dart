aa95c6ec62e2c966768372289622270dd746243f
utsav-savani <utsav.savani@gmail.com>
2026-01-03 17:01:20 +0530

    GREEN: parse and return single number

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,6 @@ class StringCalculator {
   /// Adds numbers in the given string and returns the sum.
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    return 0;
+    return int.parse(numbers);
   }
 }
