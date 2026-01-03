4a9dcb20f81d81ac83934948e43763e941104dbf
utsav-savani <utsav.savani@gmail.com>
2026-01-03 17:04:11 +0530

    GREEN: parse custom delimiter from //[delim] prefix

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,8 +4,17 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    var normalized = numbers.replaceAll('\n', ',');
-    var parts = normalized.split(',');
+    var delimiter = ',';
+    var numString = numbers;
+
+    if (numbers.startsWith('//')) {
+      var newlineIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, newlineIndex);
+      numString = numbers.substring(newlineIndex + 1);
+    }
+
+    var normalized = numString.replaceAll('\n', delimiter);
+    var parts = normalized.split(delimiter);
     return parts.map((p) => int.parse(p)).fold(0, (a, b) => a + b);
   }
 }
