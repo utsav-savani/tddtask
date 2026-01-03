97ea5be758a3af7fae9f3c8ba3a5d9947ed6136d
utsav-savani <utsav.savani@gmail.com>
2026-01-03 17:01:09 +0530

    RED: test single number returns itself

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -6,4 +6,9 @@ void main() {
     var calc = StringCalculator();
     expect(calc.add(''), 0);
   });
+
+  test('returns the number for single number', () {
+    var calc = StringCalculator();
+    expect(calc.add('1'), 1);
+  });
 }
